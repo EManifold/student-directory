263798330ccf35de5928f3c209a06e0fbd2c0852
Ellie <elliemmanifold@gmail.com>
2019-11-01 15:58:56 +0000

    Create table format, add text colour, add hobby & height options

--- directory.rb
+++ directory.rb
@@ -1,11 +1,15 @@
-@students = [] # an empty array accessible to all methods
+require 'colorize'
+
+@students = [{name: 'John', cohort: 'October', hobby: 'Fishing', height: "5'11''"}, {name: 'Alison', cohort: 'September', hobby: 'Bowling', height: "5'5''"}, {name: 'Raymond', cohort: 'September', hobby: 'Piano', height: "6'1''"}, {name: 'Julie', cohort: 'October', hobby: 'Football', height: "5'10''"}] # an empty array accessible to all methods
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "Please select an option".white
+  puts "1. Input the students".white
+  puts "2. Show the students".white
+  puts "3. Save the list to students.csv".white
+  puts "4. Load the list from students.csv".white
+  puts "5. Add more details to a student".white
+  puts "9. Exit".white # 9 because we'll be adding more items
 end
 
 def interactive_menu
@@ -25,6 +29,8 @@ def process(selection)
     save_students
   when "4"
     load_students
+  when "5"
+    add_info_to_student
   when "9"
     exit # this will cause the program to terminate
   else
@@ -40,7 +46,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :November, hobby: 'Unknown', height: 'Unknown'}
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -54,18 +60,57 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "=".center(100, '=').light_cyan
+  puts "The students of Villains Academy".center(100).light_cyan
+  puts "=".center(100, '=').light_cyan
 end
 
 def print_student_list
   @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{@students.index(student) + 1} ||".center(10).light_magenta + "#{student[:name]}".center(15).light_magenta + "#{student[:cohort]} cohort".center(20).light_magenta + "#{student[:hobby]}".center(15).light_magenta + "#{student[:height]}".center(15).light_magenta + "ID: #{student.object_id}".center(15).light_magenta
   end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts '='.center(100, '=').light_cyan
+  puts "Overall, we have #{@students.count} great students".center(100).light_red
+end
+
+def add_info_to_student
+  #Modify the chosen student
+  puts "Please provide the student's ID".light_yellow
+  student_id = gets.chomp
+
+  puts "What would you like to add? Please type 'hobby' or 'height'".light_yellow
+  addition = gets.chomp
+
+  if addition == 'hobby'
+    puts "What is their hobby?".light_yellow
+    hobby = gets.chomp
+
+    @students.each do |student|
+      if student.object_id == student_id.to_i
+        student[:hobby] = hobby
+      end
+    end
+
+    puts "Thankyou, their information has been added".light_green
+
+  elsif addition == 'height'
+    puts "What is their height?".light_yellow
+    height = gets.chomp
+
+    @students.each do |student|
+      if student.object_id == student_id.to_i
+        student[:height] = height
+      end
+    end
+
+    puts "Their information has been added".light_green
+
+  end
+
+
 end
 
 def save_students
